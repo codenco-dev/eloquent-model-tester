d1d49ba89b2ff51eb09febdb0c602166d727566e
DThomas <dthomas@codenco.fr>
2022-02-25 13:22:11 +0100

    wip Manage ternaire

--- src/ModelTester.php
+++ src/ModelTester.php
@@ -3,6 +3,7 @@
 namespace CodencoDev\EloquentModelTester;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Schema;
@@ -26,7 +27,7 @@ class ModelTester extends TestCase
     public function getModel(): ?string
     {
         throw_if(
-            is_null($this->tested) || ! $this->isModelClass($this->tested),
+            is_null($this->tested) || !$this->isModelClass($this->tested),
             new \Exception('You have to use a Eloquent Model')
         );
 
@@ -36,7 +37,7 @@ class ModelTester extends TestCase
     public function getTable(): string
     {
         throw_if(
-            ! $this->isExistingTable($this->table),
+            !$this->isExistingTable($this->table),
             new \Exception('You have to use an existing table')
         );
 
@@ -45,7 +46,7 @@ class ModelTester extends TestCase
 
     public function getModelTable(): string
     {
-        if (! empty($this->table)) {
+        if (!empty($this->table)) {
             return $this->table;
         }
 
@@ -56,7 +57,7 @@ class ModelTester extends TestCase
 
     public function isModelClass(?string $modelClass = null): bool
     {
-        if (! is_null($modelClass)) {
+        if (!is_null($modelClass)) {
             return (new $modelClass) instanceof Model;
         } else {
             return (new $this->tested) instanceof Model;
@@ -65,7 +66,7 @@ class ModelTester extends TestCase
 
     public function isExistingTable(?string $tableName = null): bool
     {
-        if (! is_null($tableName)) {
+        if (!is_null($tableName)) {
             return Schema::hasTable($tableName);
         } else {
             return Schema::hasTable($this->getModelTable());
@@ -106,7 +107,7 @@ class ModelTester extends TestCase
         $modelObject = new $modelClass;
         $notFillable = collect([]);
         foreach ($columns as $column) {
-            if (! $modelObject->isFillable($column)) {
+            if (!$modelObject->isFillable($column)) {
                 $notFillable->push($column);
             }
         }
@@ -154,7 +155,16 @@ class ModelTester extends TestCase
         $modelInstance = $this->getModel()::factory()->create();
 
         try {
-            $relatedInstance = $modelInstance->{$relation}()->save($related::factory()->make($defaultRelatedValue));
+            $relatedInstance = $modelInstance->{$relation}()->save($related::factory()->make(collect($defaultRelatedValue ?? [])->map(function ($item) {
+                if ($item instanceof Factory) {
+                    return ($item->newModel())::factory()->create()->id;
+                }
+                if (is_array($item)) {
+                    [$factory, $attributes] = $item;
+                    return ($factory->newModel())::factory()->create($attributes)->id;
+                }
+                return $item;
+            })->toArray()));
             $modelInstance->refresh();
 
             $this->assertTrue($modelInstance->{$relation}->contains($relatedInstance));
@@ -173,14 +183,15 @@ class ModelTester extends TestCase
     }
 
     public function assertHasHasManyThroughRelation(
-        string $related,
-        string $through,
+        string  $related,
+        string  $through,
         ?string $relation = null,
         ?string $firstKey = null,
         ?string $secondKey = null,
         ?string $localKey = null,
         ?string $secondLocalKey = null,
-    ): self {
+    ): self
+    {
         $relation = $relation ?: $this->getHasManyRelationName($related);
 
         $modelInstance = $this->getModel()::factory()->create();
@@ -247,7 +258,13 @@ class ModelTester extends TestCase
         $relatedInstance = $related::factory()->create();
 
         try {
-            $modelInstance->{$relation}()->attach($relatedInstance, $pivot_value ?? []);
+
+            $modelInstance->{$relation}()->attach($relatedInstance, collect($pivot_value ?? [])->map(function ($item) {
+                if ($item instanceof Factory) {
+                    return ($item->newModel())::factory()->create()->id;
+                }
+                return $item;
+            })->toArray());
 
             $this->assertTrue($modelInstance->{$relation}->contains($relatedInstance));
             $this->assertEquals($relatedInstance->id, $modelInstance->{$relation}->first()->id);
@@ -327,7 +344,7 @@ class ModelTester extends TestCase
 
     /**
      * @param $groups
-     * @param  mixed  $columns
+     * @param mixed $columns
      * @return array
      */
     public function getArrayParameters(array|string ...$args): array
@@ -335,7 +352,7 @@ class ModelTester extends TestCase
         $params = null;
         foreach ($args as $arg) {
             $params = array_merge(
-                (array) $params,
+                (array)$params,
                 Arr::wrap($arg)
             );
         }
@@ -345,6 +362,6 @@ class ModelTester extends TestCase
 
     private function getMorphs(string $name, ?string $type, ?string $id): array
     {
-        return [$type ?: $name.'_type', $id ?: $name.'_id'];
+        return [$type ?: $name . '_type', $id ?: $name . '_id'];
     }
 }
