baa9476e1ec056149ad3649de771e516427a567a
Richard Browne <r.a.browne@gmail.com>
2022-09-30 17:56:36 +0100

    💅 StyleCI correction.

--- tests/TestModels/SixthModel.php
+++ tests/TestModels/SixthModel.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace CodencoDev\EloquentModelTester\Tests\TestModels;
-use Database\Factories\SixthModelFactory;
 
+use Database\Factories\SixthModelFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SixthModel extends Model
 {
