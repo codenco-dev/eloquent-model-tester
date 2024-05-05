aeae54f07c842b3cdcf2dc0ca497473c715aade9
Dominique Thomas <dthomas@codenco.fr>
2024-05-05 16:06:02 +0200

    mise à jour Laravel 11

--- tests/EloquentModelTest.php
+++ tests/EloquentModelTest.php
@@ -16,10 +16,8 @@ class EloquentModelTest extends TestCase
 {
     use HasModelTester;
 
-    /**
-     * @test
-     */
-    public function it_have_first_model_model()
+    
+    public function test_it_have_first_model_model()
     {
         $this->modelTestable(FirstModel::class)
             ->assertHasColumns('id', 'name')
@@ -33,19 +31,15 @@ class EloquentModelTest extends TestCase
             ->assertHasHasOneRelation(SixthModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_have_first_model_model_without_relation_parameter()
+    
+    public function test_it_have_first_model_model_without_relation_parameter()
     {
         $this->modelTestable(FirstModel::class)
             ->assertHasHasManyRelation(SecondModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_have_second_model_model()
+    
+    public function test_it_have_second_model_model()
     {
         $column = ['id', 'name', 'first_model_id'];
         $this->modelTestable(SecondModel::class)
@@ -58,20 +52,16 @@ class EloquentModelTest extends TestCase
             ->assertHasHasManyMorphRelation(MorphModel::class, 'morph_models');
     }
 
-    /**
-     * @test
-     */
-    public function it_have_second_model_model_without_relation_parameter()
+    
+    public function test_it_have_second_model_model_without_relation_parameter()
     {
         $this->modelTestable(SecondModel::class)
             ->assertHasBelongsToRelation(FirstModel::class)
             ->assertHasManyToManyRelation(ThirdModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_have_third_model_model()
+    
+    public function test_it_have_third_model_model()
     {
         $this->modelTestable(ThirdModel::class)
             ->assertHasColumns(['id', 'name'])
@@ -79,10 +69,8 @@ class EloquentModelTest extends TestCase
             ->assertHasManyToManyRelation(SecondModel::class, 'second_models');
     }
 
-    /**
-     * @test
-     */
-    public function it_have_fifth_model_model()
+    
+    public function test_it_have_fifth_model_model()
     {
         $this->modelTestable(FifthModel::class)
             ->assertHasColumns(['id', 'name'])
@@ -92,10 +80,8 @@ class EloquentModelTest extends TestCase
             ->assertHasOnlyColumnsInGuarded('is_admin');
     }
 
-    /**
-     * @test
-     */
-    public function it_tests_for_the_timestamps()
+    
+    public function test_it_tests_for_the_timestamps()
     {
         $this->modelTestable(SixthModel::class)
             ->assertHasColumns(['id', 'name'])
@@ -103,120 +89,96 @@ class EloquentModelTest extends TestCase
             ->assertHasSoftDeleteTimestampColumns();
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_fillable_field_is_not_exactly_as_expected()
+    
+    public function test_it_fails_when_the_fillable_field_is_not_exactly_as_expected()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(FifthModel::class)
             ->assertCanOnlyFill(['id', 'name', 'second_model_id', 'is_admin']);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_table_columns_is_not_exactly_as_expected()
+    
+    public function test_it_fails_when_the_table_columns_is_not_exactly_as_expected()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(FifthModel::class)
             ->assertHasOnlyColumns(['id', 'name', 'second_model_id', 'is_admin', 'missing']);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_guarded_field_is_not_exactly_as_expected()
+    
+    public function test_it_fails_when_the_guarded_field_is_not_exactly_as_expected()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(FifthModel::class)
             ->assertHasOnlyColumnsInGuarded(['id', 'is_admin']);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_guarded_field_does_not_include_the_expected_value()
+    
+    public function test_it_fails_when_the_guarded_field_does_not_include_the_expected_value()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(FifthModel::class)
             ->assertHasColumnsInGuarded(['id']);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_fillable_field_does_not_include_the_expected_value()
+    
+    public function test_it_fails_when_the_fillable_field_does_not_include_the_expected_value()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(FifthModel::class)
             ->assertHasColumnsInFillable(['is_admin']);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_guarded_and_fillable_arrays_contain_the_same_value()
+    
+    public function test_it_fails_when_the_guarded_and_fillable_arrays_contain_the_same_value()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasNoGuardedAndFillableFields();
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_guarded_and_fillable_arrays_contain_the_same_value_when_asserting_only_fillable()
+    
+    public function test_it_fails_when_the_guarded_and_fillable_arrays_contain_the_same_value_when_asserting_only_fillable()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertCanOnlyFill('id', 'name', 'is_admin');
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_one_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_one_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasHasOneRelation(FifthModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_morph_one_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_morph_one_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasMorphOneRelation(FirstModel::class, 'morph_models');
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_many_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_many_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasHasManyRelation(FirstModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_many_through_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_many_through_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasHasManyThroughRelation(FirstModel::class, SecondModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_belongs_to_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_belongs_to_relation()
     {
         FirstModel::factory()->create();
 
@@ -225,30 +187,24 @@ class EloquentModelTest extends TestCase
             ->assertHasBelongsToRelation(FifthModel::class, null, 'first_model_id');
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_many_to_many_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_many_to_many_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasManyToManyRelation(FirstModel::class);
     }
 
-    /**
-     * @test
-     */
-    public function it_fails_when_the_model_does_not_have_the_has_many_morph_relation()
+    
+    public function test_it_fails_when_the_model_does_not_have_the_has_many_morph_relation()
     {
         $this->expectException(ExpectationFailedException::class);
         $this->modelTestable(SixthModel::class)
             ->assertHasHasManyMorphRelation(FirstModel::class, 'morph_models');
     }
 
-    /**
-     * @test
-     */
-    public function it_have_morph_model_model()
+    
+    public function test_it_have_morph_model_model()
     {
         $this->modelTestable(MorphModel::class)
             ->assertHasColumns(['id', 'name', 'morph_modelable_type', 'morph_modelable_id'])
@@ -257,10 +213,8 @@ class EloquentModelTest extends TestCase
             ->assertHasBelongsToMorphRelation(SixthModel::class, 'morph_modelable');
     }
 
-    /**
-     * @test
-     */
-    public function it_have_sixth_model_model()
+    
+    public function test_it_have_sixth_model_model()
     {
         $column = ['id', 'name', 'first_model_id'];
         $this->modelTestable(SixthModel::class)
@@ -271,19 +225,15 @@ class EloquentModelTest extends TestCase
             ->assertHasMorphOneRelation(MorphModel::class, 'morphed');
     }
 
-    /**
-     * @test
-     */
-    public function it_have_table_without_model()
+    
+    public function test_it_have_table_without_model()
     {
         $this->tableTestable('second_model_third_model')
             ->assertHasColumns(['second_model_id', 'third_model_id']);
     }
 
-    /**
-     * @test
-     */
-    public function it_have_scope_in_first_model()
+    
+    public function test_it_have_scope_in_first_model()
     {
         $this->modelTestable(FirstModel::class)
             ->assertHasScope('theScope1')
--- tests/UnitModelTesterTest.php
+++ tests/UnitModelTesterTest.php
@@ -9,19 +9,15 @@ use CodencoDev\EloquentModelTester\Tests\TestModels\SecondModel;
 
 class UnitModelTesterTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function it_doesnt_run_without_model_class()
+    
+    public function test_it_doesnt_run_without_model_class()
     {
         $this->assertFalse(ModelTester::create(NotAModel::class)->isModelClass());
         $this->assertTrue(ModelTester::create(FirstModel::class)->isModelClass());
     }
 
-    /**
-     * @test
-     */
-    public function it_doesnt_run_without_existing_table_name()
+    
+    public function test_it_doesnt_run_without_existing_table_name()
     {
         $this->assertFalse(ModelTester::create(null, 'not_exists')->isExistingTable());
         $this->assertTrue(ModelTester::create(null, 'first_models')->isExistingTable());
@@ -29,30 +25,24 @@ class UnitModelTesterTest extends TestCase
         $this->assertTrue(ModelTester::create(FirstModel::class)->isExistingTable());
     }
 
-    /**
-     * @test
-     */
-    public function it_should_have_belongs_to_relations_name()
+    
+    public function test_it_should_have_belongs_to_relations_name()
     {
         $instance = ModelTester::create(FirstModel::class);
         $relation = $instance->getBelongsToRelationName(SecondModel::class);
         $this->assertEquals('second_model', $relation);
     }
 
-    /**
-     * @test
-     */
-    public function it_should_have_has_many_relations_name()
+    
+    public function test_it_should_have_has_many_relations_name()
     {
         $instance = ModelTester::create(FirstModel::class);
         $relation = $instance->getHasManyRelationName(SecondModel::class);
         $this->assertEquals('second_models', $relation);
     }
 
-    /**
-     * @test
-     */
-    public function it_should_have_many_to_many_relations_name()
+    
+    public function test_it_should_have_many_to_many_relations_name()
     {
         $instance = ModelTester::create(FirstModel::class);
         $relation = $instance->getManyToManyRelationName(SecondModel::class);
